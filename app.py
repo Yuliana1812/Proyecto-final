a5035e08004dee41354efd02cb2292d3785e1bc0
Yuliana1812 <yulianabarbosa18@gmail.com>
2025-05-21 21:07:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -116,15 +116,18 @@ def mostrar_estadisticas(df, columna, titulo):
 
 
 with tab1:
-    st.header("Análisis Univariado de Indicadores Económicos")
-    st.write("Esta sección muestra la distribución estadística de cada indicador económico.")
+    st.header("Análisis univariado")
+    st.subheader("Palabras más pronunciadas por el presidente en sus discursos")
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.image("imagen1.png", caption="Análisis de frecuencia")
+        st.image("imagen1.png", caption="Análisis de frecuencia discurso 1")
     with col2:
-        st.image("imagen2.png", caption="Análisis de frecuencia")
+        st.image("imagen2.png", caption="Análisis de frecuencia discurso 2")
     with col3:
-        st.image("imagen3.png", caption="Análisis de frecuencia")
+        st.image("imagen3.png", caption="Análisis de frecuencia discurso 3")
+        
+    st.subheader("Análisis Univariado de Indicadores Económicos")
+
 
     indicador = st.selectbox(
         "Seleccione un indicador para ver su análisis univariado:",
